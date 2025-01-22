58686a5f3e73715245ca3fe058b9e6b2c5028b39
NisargMajithiya <nisarg02100210@gmail.com>
2025-01-22 12:34:02 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+/*let score = null 
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// "33=> 33"
+// "33abc=> Nan
+// "true => 1 ,false => 0
+
+*/
+let isloggedin = "Nisarg"
+
+let booleanisloggedin = Boolean(isloggedin)
+
+console.log(booleanisloggedin);
+
+
+// 1=> true || 0 => false
+//Nisarg => true
+
+
+let somenumber =33
+
+let stringnumber= String(somenumber)
+
+console.log(typeof somenumber);
+
+console.log(typeof stringnumber);
+
+console.log(stringnumber);
+
+
+
