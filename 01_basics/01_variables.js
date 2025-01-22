d65bd7d082083fbea5bd086a0d27021b8555c0f2
NisargMajithiya <nisarg02100210@gmail.com>
2025-01-22 11:23:29 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1234
+let accountEmail = "nsiarg@gmail.com"
+
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+let accountState
+
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+
+accountPassword = "213456"
+
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
