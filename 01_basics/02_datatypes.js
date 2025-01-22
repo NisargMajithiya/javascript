654c8e9f7e9b907353933b61c5dc2b980ed820a9
NisargMajithiya <nisarg02100210@gmail.com>
2025-01-22 12:14:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all Js code as never version
+
+// alert(4+4) we are using node js
+
+console.log(3+3);
+
+console.log("nisarg")
+
+let name = "Nisarg"
+let age = 18
+let isloggedin = false
+let State;
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+//boolean => true/false
+// null standalone value 
+// undefined =>  
+// Symbol
+
+
+//object 
+console.log(typeof undefined);
+
+
+
