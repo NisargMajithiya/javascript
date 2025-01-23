83ff579164684360a01667f9995eb044b479a64e
NisargMajithiya <nisarg02100210@gmail.com>
2025-01-23 14:20:35 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+
 /*let score = null 
 
 console.log(typeof score);
@@ -14,17 +15,21 @@ console.log(valueInNumber);
 // "true => 1 ,false => 0
 
 */
+
+/*
 let isloggedin = "Nisarg"
 
 let booleanisloggedin = Boolean(isloggedin)
 
 console.log(booleanisloggedin);
 
+*/
+
 
 // 1=> true || 0 => false
 //Nisarg => true
 
-
+/*
 let somenumber =33
 
 let stringnumber= String(somenumber)
@@ -34,6 +39,33 @@ console.log(typeof somenumber);
 console.log(typeof stringnumber);
 
 console.log(stringnumber);
+*/
+
+let value = 3
+
+let negvalue = -value 
+//console.log(negvalue);
+
+let str1 = "Nisarg"
+
+let str2 = "majithiya"
+
+console.log(1+2+"1");
+
+console.log("1"+3);
+
+console.log(+true); 
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+
+
 
 
 
