c918cc50c2d24d1627a03f3dcdb52d3486713d69
NisargMajithiya <nisarg02100210@gmail.com>
2025-01-29 14:50:46 +0000

    datatypes-summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,45 @@
+// primitive 7
+
+//    string, Number, boolean, null, undefined, symbol, Bigint 
+
+
+const score = false 
+
+// Reference / non-primitive datatype
+
+// Array, objects, Functions 
+
+const scorevalue = 100.3
+
+const isloggedin = false
+const outsideTemp = null 
+let userEmail = undefined;
+
+const Id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+//console.log(Id === anotherId);
+
+const bignumber = 124523652485454125258n
+
+//console.log(typeof(bignumber));
+
+
+const heros =  ["shaktiman", "naagraj", "doga"]
+
+let myobj = {
+    name : "nisarg",
+    age : 20,
+}
+
+
+const myfunction  = function(){
+
+    console.log("hello world");
+
+}
+
+console.log(typeof(myfunction));
+
+
