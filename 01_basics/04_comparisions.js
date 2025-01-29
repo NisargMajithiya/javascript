b01d640cbf1be79ff69d71c69567599879e75595
NisargMajithiya <nisarg02100210@gmail.com>
2025-01-29 14:24:24 +0000

    comparision

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,31 @@
+// console.log(2>1);
+// console.log(2>1);
+// console.log(2>1);
+// console.log(2>1);
+// console.log(2>1);
+
+
+//console.log("02" > 4);
+//console.log("2" > 4);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(nulll >= 0);
+
+//console.log(undefined == 0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+ 
