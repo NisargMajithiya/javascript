1eb527cb09eb2f401aecf89ced8f9653bc967625
NisargMajithiya <nisarg02100210@gmail.com>
2025-02-03 14:46:41 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,49 @@
+const name = "nisarg"
+
+const repopcount = 1
+
+console.log(name + repopcount + "value");
+
+
+console.log(`hello my name is ${name} and my repo count is ${repopcount} `);
+
+
+const gamename = new String('nisarg-majithiya')
+
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(5));
+
+console.log(gamename.indexOf('g'));
+
+
+const newvariable = gamename.substring(0,6)
+console.log(newvariable);
+
+
+
+
+const newstringone = "    nisarg  "
+
+console.log(newstringone);
+console.log(newstringone.trim());
+
+
+const url = "bdfkjssidfjlirjglsd//er%20f.com"
+
+// urln coding
+
+
+console.log(url.replace('%20','-'));
+  
+
+console.log(url.includes('new'));
+
+console.log(gamename.split());
+
+
+
