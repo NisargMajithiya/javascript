bf0b16a5f15abaa00dbef0aa8154714984b70cb8
NisargMajithiya <nisarg02100210@gmail.com>
2025-02-03 15:40:25 +0000

    Math.

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,43 @@
+const score = 400
+//console.log(400);
+
+const balance = new Number(100)
+
+//console.log(balance);
+
+//console.log(balance.toString().length);
+
+//console.log(balance.toFixed(2));
+
+const othernumber = 123.7886
+
+//console.log(othernumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//   math
+
+
+console.log(Math);
+
+
+console.log(Math.round(4.33));
+
+
+console.log((Math.random()*10) + 1);
+
+const min = 100
+const max = 200
+
+
+console.log(Math.floor(Math.random() * (max - min + 1 ) + min));
+
+
+
+
+
+
+
